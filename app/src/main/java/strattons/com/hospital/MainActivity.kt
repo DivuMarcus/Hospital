89dbaf0958fd8598834f82433a0da42c355693f6
DivuMarcus <36792228+DivuMarcus@users.noreply.github.com>
2018-09-26 02:28:09 +0530

    New One

--- app/src/main/java/strattons/com/hospital/MainActivity.kt
+++ app/src/main/java/strattons/com/hospital/MainActivity.kt
@@ -1,12 +1,25 @@
 package strattons.com.hospital
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    var new: Button? = null
+    var existButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        existButton = findViewById(R.id.ButtonBottom)
+
+        existButton?.setOnClickListener({
+            var clickIntent = Intent(this@MainActivity, DetailActivity::class.java)
+            startActivity(clickIntent)
+        })
+
+
     }
 }
